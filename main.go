ca21ae8d4c1b64f3632b1c600605a7f07368bf90
Hana Ra <hanara@usc.edu>
2022-05-31 18:03:45 -0700

    added if-else and switch

--- main.go
+++ main.go
@@ -1,21 +1,21 @@
 // the file name main.go is not optional; it's what gets compiled; it's entry point
 package main
+
 import (
 	"fmt"
 	"strings"
 )
 
-
 func mul(a int, b int) int {
-	return a*b
+	return a * b
 }
-
 
 // a func can return many things
 func lenAndUpper(name string) (int, string) {
 	defer fmt.Println("lenAndUpper function done!") // defer gets executed after this func returns
 	return len(name), strings.ToUpper(name)
 }
+
 // "naked return" way
 // func lenAndUpper(name string) (namelen int, upper string) { // here defind that I will return len and upper
 // 	defer fmt.Println("naked lenAndUpper done!")
@@ -24,12 +24,10 @@ func lenAndUpper(name string) (int, string) {
 // 	return // naked return!
 // }
 
-
 // take any number of args
 func manyargs(args ...string) {
 	fmt.Println(args) // prints in a form of array!
 }
-
 
 func sumAll(nums ...int) int {
 	// nums is an array
@@ -47,32 +45,74 @@ func sumAll(nums ...int) int {
 	return sum
 }
 
+func candrink(age int) bool {
+	if age > 18 {
+		return true
+	} // else
+	return false
+}
 
+func candrinkKOR(age int) bool {
+	// create var right inside of if-stmt; "variable expression"
+	// I can create "koreanAge := age + 2" outside of if-stmt too
+	// but doing this way indicates that I am using koreanAge var for if-stmt only
+	if koreanAge := age + 2; koreanAge > 18 {
+		return true
+	} // else
+	return false
+}
 
+func candrinkSwitch(age int) bool {
+	switch age {
+	case 10:
+		return false
+	case 18:
+		return true
+	}
+	return false
+}
+
+func candrinkSwitch2(age int) bool {
+	switch {
+	case age > 50:
+		return false
+	case age > 18:
+		return true
+	}
+	return false
+}
 
 // this main func is mandatory
 func main() {
-//// PRINT
+	//// PRINT
 	// fmt.Println("hello world")
-	
-//// DEFIND CONST AND VARS
+
+	//// DEFIND CONST AND VARS
 	const name string = "hana" // constants, can't change
 	// var fruit string = "apple" // varaible
 	// fruit = "banana" // can redefind var
 	// car := "subaru" // shorthand  way of defining a var
 
-//// FUNCTIONS 
+	//// FUNCTIONS
 	// fmt.Println(mul(2, 2))
 
-//// GET MORE THAN ONE RETURN VAL
+	//// GET MORE THAN ONE RETURN VAL
 	length, uppercase := lenAndUpper(name)
 	// length, _ := lenAndUpper(name) // to ignore uppercase return val
 	fmt.Println(length, uppercase)
 
-//// FUNCTIONS THAT TAKES ANY NUMBER OF STRING ARGS
+	//// FUNCTIONS THAT TAKES ANY NUMBER OF STRING ARGS
 	manyargs("hana", "ravi", "junco", "ninja", "scroll")
 
-//// Forloop
+	//// Forloop
 	fmt.Println(sumAll(1, 2, 3, 4, 5))
+
+	//// IF, ELSE
+	fmt.Println(candrink(17))
+	fmt.Println(candrinkKOR(17))
+
+	//// SWITCH
+	fmt.Println("candrinkSwitch: ", candrinkSwitch(17))
+	fmt.Println("candrinkSwitch2: ", candrinkSwitch2(37))
 
 }
